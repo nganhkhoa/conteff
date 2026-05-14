ced0d182973758feb1171328b12399b25dc620f2
nganhkhoa <service.nganhkhoa@gmail.com>
2026-05-14 00:02:23 -0500

    update box vector contract so that it breaks

--- test/tmp/box.ml
+++ test/tmp/box.ml
@@ -9,19 +9,20 @@ let smaller_20 x = x < 20
    placing contracts on tuple is also possible
  *)
 let [@contract
-  : (((any -> any) * (any -> any) as 'tup)
+  : (((any -> smaller_20) * (any -> any) as 'tup)
   * ((any -> any) * (any -> any) as 'tup))
   -> any]
   process (box1 : int Box.t) (box2 : int Box.t) : unit =
   let v1 = Box.get box1 in
+  Printf.printf "box1: before=%d\n" v1;
   let _ = Box.set box1 (v1 + 100) in
   let v3 = Box.get box1 in
-  Printf.printf "box1: before=%d after=%d\n" v1 v3;
+  Printf.printf "box1: after=%d\n" v3;
 
-  let v1 = Box.get box2 in
+  (* let v1 = Box.get box2 in
   let _ = Box.set box2 (v1 * 100) in
   let v3 = Box.get box2 in
-  Printf.printf "box2: before=%d after=%d\n" v1 v3;
+  Printf.printf "box2: before=%d after=%d\n" v1 v3; *)
   ()
 
 let main () =
--- test/tmp/vector.ml
+++ test/tmp/vector.ml
@@ -9,13 +9,14 @@ let smaller_20 x = x < 20
    placing contracts on tuple is also possible
  *)
 let [@contract
-  : ((any -> any) * (any -> any -> any) as 'tup)
+  : ((smaller_20 -> bigger_10) * (smaller_20 -> any -> any) as 'tup)
   -> any]
   process (vec : int Vector.t) : unit =
   let v1 = Vector.get vec 10 in
-  let _ = Vector.set vec 10 (v1 + 100) in
+  Printf.printf "vec[10]: before=%d\n" v1;
+  let _ = Vector.set vec 10 9 in
   let v3 = Vector.get vec 10 in
-  Printf.printf "vec[10]: before=%d after=%d\n" v1 v3;
+  Printf.printf "vec[10]: after=%d\n" v3;
 
   let v1 = Vector.get vec 5 in
   let _ = Vector.set vec 5 (v1 + 1000) in
@@ -29,7 +30,7 @@ let main () =
   let cloc = "process" in
   let process = process pos neg cloc in
 
-  let (vec_handler, vec) = Vector.create 20 6 in
+  let (vec_handler, vec) = Vector.create 20 11 in
   vec_handler (fun () -> process vec)
 
 let () =
